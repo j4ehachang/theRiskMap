e691fc0a9ec36169b2490d02593d173fe2225243
Jaeha <jcha513@aucklanduni.ac.nz>
2024-05-24 18:21:03 +1200

    Created a method in the graph class to find shortest path using the BFS

--- src/main/java/nz/ac/auckland/se281/Graph.java
+++ src/main/java/nz/ac/auckland/se281/Graph.java
@@ -2,8 +2,10 @@ package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class Graph {
   private Map<Country, List<Country>> adjNodes;
@@ -20,7 +22,6 @@ public class Graph {
     addNode(node1);
     addNode(node2);
     adjNodes.get(node1).add(node2);
-    adjNodes.get(node2).add(node1);
   }
 
   public void removeNode(Country node) {
@@ -32,6 +33,29 @@ public class Graph {
 
   public void removeEdge(Country node1, Country node2) {
     adjNodes.getOrDefault(node1, new ArrayList<>()).remove(node2);
-     adjNodes.getOrDefault(node2, new ArrayList<>()).remove(node1);
-   }
+    adjNodes.getOrDefault(node2, new ArrayList<>()).remove(node1);
+  }
+
+  public List<Country> findShortestPath(Country root) {
+    if (!adjNodes.containsKey(root)) {
+      return null; // Start node not present in the graph
+    }
+
+    List<Country> visited = new ArrayList<>();
+    Queue<Country> queue = new LinkedList<>();
+    Map<Country, Country> parentMap = new HashMap<>();
+
+    queue.add(root);
+    visited.add(root);
+    while (!queue.isEmpty()) {
+      Country node = queue.poll();
+      for (Country n : adjNodes.get(node)) {
+        if (!visited.contains(n)) {
+          visited.add(n);
+          queue.add(n);
+        }
+      }
+    }
+    return visited;
+  }
 }
